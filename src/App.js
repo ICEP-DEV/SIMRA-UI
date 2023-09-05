079f4abf9f542de2a535ca4e24b8095629a625ce
Ngoy <220904814@tut4life.ac.za>
2023-09-05 12:02:09 +0200

    login implementation

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
-import NavBar from './NavBar';
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
-import { BrowserRouter as Router, Route, Link,Routes } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Login from './Components/Auth/Login/Login';
+//import RegisterForm from './Components/Auth/Registration/RegisterForm';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Router>
-        <nav className="navbar">
-          <div className="navbar-left">
-            <div className="logo">Logo</div>
-          </div>
-          <div className="navbar-right">
-            <ul className="nav-links">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="./about">About</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
-              <li><a href="#">Sign In</a></li>
-            </ul>
-          </div>
-        </nav>
-        <Routes>
-          <Route exact path="/" element={Home} />
-          <Route path="/about" element={About} />
-          <Route path="/contact" element={Contact} />
-        </Routes>
-      </Router>
-    </div>
+    <BrowserRouter>
+    <Routes>
+   
+   <Route path='/' element={<Login/>}></Route>
+  
+
+
+    </Routes>
+    
+    </BrowserRouter>
   );
 }
+
 export default App;
