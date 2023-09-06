19741d65603da69c6a4fcbdaacccbda39c5423e8
busisilawule <bsilawule98@gmail.com>
2023-09-06 11:14:27 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,69 +1,53 @@
 import logo from './logo.svg';
 import './index.css';
 
-const fs = require('fs');
-const usersData = require('./users.json');
 
-const registrationForm = document.getElementById('registration-form');
-const emailVerificationCodeInput = document.getElementById('email-verification-code');
-const mobileVerificationCodeInput = document.getElementById('mobile-verification-code');
-
-registrationForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const firstName = document.getElementById('reg-firstname').value;
-    const lastName = document.getElementById('reg-lastname').value;
-    const email = document.getElementById('reg-email').value;
-    const mobile = document.getElementById('reg-mobile').value;
-    // ... other registration form data ...
-
-    // Generate random 6-digit verification codes for email and mobile
-    const emailVerificationCode = Math.floor(100000 + Math.random() * 900000);
-    const mobileVerificationCode = Math.floor(100000 + Math.random() * 900000);
-
-    // Send verification codes to the user's email and mobile (not shown in this example)
-
-    // Add registration data to JSON file
-    usersData.users.push({
-        firstName,
-        lastName,
-        email,
-        mobile,
-        // ... other registration data ...
-        emailVerificationCode,
-        mobileVerificationCode
-    });
-
-    // Update users.json with new data
-    fs.writeFileSync('./users.json', JSON.stringify(usersData, null, 2));
-
-    // Show verification code input fields
-    emailVerificationCodeInput.style.display = 'block';
-    mobileVerificationCodeInput.style.display = 'block';
-    alert('Verification codes have been sent to your email and mobile. Please enter them to complete the registration.');
-});
-
-emailVerificationCodeInput.addEventListener('input', () => {
-    const enteredVerificationCode = emailVerificationCodeInput.value;
-    validateVerificationCode(enteredVerificationCode, 'emailVerificationCode');
-});
-
-mobileVerificationCodeInput.addEventListener('input', () => {
-    const enteredVerificationCode = mobileVerificationCodeInput.value;
-    validateVerificationCode(enteredVerificationCode, 'mobileVerificationCode');
-});
-
-function validateVerificationCode(enteredCode, storedCodeKey) {
-    if (enteredCode.length === 6) {
-        const registeredUser = usersData.users.find(user => user.email === email);
-
-        if (registeredUser && parseInt(enteredCode) === registeredUser[storedCodeKey]) {
-            // Verification successful
-            alert('Verification successful!');
-        } else {
-            alert('Verification code is incorrect. Please try again.');
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    const verificationForm = document.getElementById('verification-form');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const verificationCodeInput = document.getElementById('verification-code');
+    const verificationStatus = document.getElementById('verification-status');
+    
+    // Function to send a verification code (simulated)
+    function sendVerificationCode(type, destination) {
+        const code = generateVerificationCode();
+        alert(`Sending ${type} verification code: ${code} to ${destination}`);
     }
-}
-
-
+    
+    // Function to generate a random 6-digit code
+    function generateVerificationCode() {
+        return Math.floor(100000 + Math.random() * 900000);
+    }
+    
+    document.getElementById('send-email-code').addEventListener('click', () => {
+        const email = emailInput.value;
+        if (email) {
+            sendVerificationCode('email', email);
+        }
+    });
+    
+    document.getElementById('send-phone-code').addEventListener('click', () => {
+        const phone = phoneInput.value;
+        if (phone) {
+            sendVerificationCode('phone', phone);
+        }
+    });
+    
+    verificationForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const enteredCode = verificationCodeInput.value;
+        const email = emailInput.value;
+        const phone = phoneInput.value;
+        
+        // Here, send the entered code to backend for verification.
+        // example, simulate a successful verification.
+        const simulatedVerificationCode = '123456'; // Simulated correct code
+        
+        if (enteredCode === simulatedVerificationCode) {
+            verificationStatus.textContent = 'Verification successful!';
+        } else {
+            verificationStatus.textContent = 'Verification code is incorrect. Please try again.';
+        }
+    });
+});
